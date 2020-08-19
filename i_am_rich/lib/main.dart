c0a15a08c9dabebea426c23fe16d8a4a8ef1981a
gabboraron <gabboraron@gmail.com>
2020-08-19 13:31:46 +0200

    lokális kép betöltése

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -15,7 +15,7 @@ void main() {  //starting point of app
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
