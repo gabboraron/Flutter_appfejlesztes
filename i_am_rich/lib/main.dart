46a39ffe81d8eb8fba70a8db2d99dd0961a48f31
gabboraron <gabboraron@gmail.com>
2020-08-19 12:13:47 +0200

    hello world

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,15 @@
+//I AM Rich app
+// https://material.io/ hasznalataval
+// egy egyszerű applikáció
+//
+import 'package:flutter/material.dart';
+
+void main() {  //starting point of app
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
